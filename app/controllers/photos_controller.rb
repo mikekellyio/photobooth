a2fd9909ba8c3a4a6adbbbe5eb89bdba98e421b6
Mike Kelly <mike@kellyfam.net>
2013-12-24 01:57:57 -0500

    add photos page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,14 @@
 class PhotosController < ApplicationController
   protect_from_forgery :except => :create 
+
+  def index
+    @photos = Event.current.sorted_photos
+  end
+
+  def show
+    @photo = Event.current.photos.find(params[:id])
+  end
+
   def create
     Event.add_photos
     redirect_to root_path
